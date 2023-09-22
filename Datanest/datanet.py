4fb74c5bf08db59d403c687eb995438a57ae6db2
Ethan Barr <ethanwbarr07@gmail.com>
2023-09-22 19:46:14 +0100

    Start of a function module

--- Datanest/datanet.py
+++ Datanest/datanet.py
@@ -1,24 +1,60 @@
-# module_file.py
+import os
 
-# Define a simple function
-def greet(name):
-    return f"Hello, {name}!"
 
-# Define a class
-class Calculator:
-    def add(self, a, b):
-        return a + b
+class Datasets: 
+    def __init__(self, path):
+        self.path = path
 
-    def subtract(self, a, b):
-        return a - b
 
-    def multiply(self, a, b):
-        return a * b
 
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError("Cannot divide by zero")
-        return a / b
+    def possible_models(self):
+        path = self.path
 
-# Define a global variable
-PI = 3.141592653589793
+        for root, _, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                print(file_path)
+
+    ## Transcripts
+    def load_whole_transcript(self, model):
+        path = self.path
+
+        if ".txt" in model:
+            for file_name in os.listdir(path):
+                if file_name == model:
+                    target_file_path = os.path.join(path, model)
+                    with open(target_file_path, 'r') as file:
+                        file_contents = file.read()
+
+
+                    return file_contents
+            
+            
+            else:
+                return "File not found in the specified folder."
+
+    
+    ## Character's
+    def save_character(model, output_dir, character):
+        lines = []
+    
+        with open(f"{model}", 'r') as source_flie:
+            for line in source_flie:
+                if character in line:
+                    lines.append(line)
+    
+        if lines:
+            with open(f"Characters/{output_dir}", 'w') as output:
+                for line in lines:
+                    output.write(line)
+                    return f"{character} saved in {output}"
+
+
+    def load_character(dataset):
+        data = []
+    
+        with open(f"Characters/{dataset}", 'r') as source_file:
+            for line in source_file:
+                data.append(line)
+    
+        return data
