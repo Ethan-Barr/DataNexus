cb10c94aca64b95bcec4c4e156dc95237c28262b
Ethan Barr <ethanwbarr07@gmail.com>
2023-09-23 15:51:07 +0100

    DataNexus V0.0.1
    https://pypi.org/project/DataNexus/
    
    Docs and Readme.md to be created

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Datanest
- 
+# DataNexus
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='datanest',
-    version='0.0.2',
+    name='DataNexus',
+    version='0.0.1',
     description='A dataset module for your projects',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author='Ethan Barr',
     author_email='ethanwbarr07@gmail.com',
-    url='https://github.com/Ethan-Barr/Datanest',
+    url='https://github.com/Ethan-Barr/DataNexus',
     packages=find_packages(),
     install_requires=[
     "Requests==2.28.2"
